ada5ef744d8c01175adc324bd78a0dd6c850a751
Paul <storuky@gmail.com>
2015-07-29 10:56:50 +0300

    create category

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
   has_many :users_products
 
-  has_many :users, through: :users_products 
+  has_many :users, through: :users_products
+
+  belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728174955) do
+ActiveRecord::Schema.define(version: 20150729075540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150728174955) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.float    "price"
+    t.integer  "category_id"
   end
 
   create_table "users", force: :cascade do |t|
